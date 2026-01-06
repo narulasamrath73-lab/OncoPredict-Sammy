6a5c11470a4c3e801b6ef986355939f723aee466
narulasamrath73-lab <narulasamrath73@gmail.com>
2026-01-06 14:37:50 +0530

    Added evaluation and cross validation script
    This script evaluates a logistic regression model on cancer data, including data preprocessing, model training, and performance metrics.

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,50 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, f1_score
+
+# Load dataset
+df = pd.read_csv("data/raw/cancer_data.csv")
+df.dropna(inplace=True)
+
+X = df[
+    ['Age','Gender','BMI','Smoking','GeneticRisk',
+     'PhysicalActivity','AlcoholIntake','CancerHistory']
+]
+y = df['Diagnosis']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression(class_weight='balanced', max_iter=1000)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Recall:", recall_score(y_test, y_pred))
+print("F1 Score:", f1_score(y_test, y_pred))
+
+# Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+plt.title("Confusion Matrix")
+plt.show()
+
+# Cross validation
+cv = StratifiedKFold(n_splits=9, shuffle=True, random_state=19)
+cv_scores = cross_val_score(model, X_scaled, y, cv=cv, scoring='accuracy')
+
+print("Cross Validation Scores:", cv_scores)
+print("Mean CV Accuracy:", cv_scores.mean())
 
