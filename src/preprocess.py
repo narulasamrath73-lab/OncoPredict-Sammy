6b0b64e4939e1f34c11ccb97625c2d482cb9b689
narulasamrath73-lab <narulasamrath73@gmail.com>
2026-01-06 14:55:03 +0530

    Added preprocessing logic
    This function loads cancer data, preprocesses it by removing missing values, defining features and target, and applying feature scaling.

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,31 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+def load_and_preprocess_data():
+    # Load dataset
+    df = pd.read_csv("data/raw/cancer_data.csv")
+
+    # Remove missing values
+    df.dropna(inplace=True)
+
+    # Define features and target
+    X = df[
+        [
+            'Age',
+            'Gender',
+            'BMI',
+            'Smoking',
+            'GeneticRisk',
+            'PhysicalActivity',
+            'AlcoholIntake',
+            'CancerHistory'
+        ]
+    ]
+
+    y = df['Diagnosis']
+
+    # Feature scaling
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    return X_scaled, y, scaler
 
