4eb040585cff4327880585512e0ccfec7221cd51
narulasamrath73-lab <narulasamrath73@gmail.com>
2026-01-06 14:46:47 +0530

    Added evaluation script

--- src/evaluate.py
+++ src/evaluate.py
@@ -13,16 +13,13 @@ df.dropna(inplace=True)
 
 X = df[
     ['Age','Gender','BMI','Smoking','GeneticRisk',
-     'PhysicalActivity','AlcoholIntake','CancerHistory']
-]
+     'PhysicalActivity','AlcoholIntake','CancerHistory']]
 y = df['Diagnosis']
 
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X_scaled, y, test_size=0.2, random_state=42
-)
+X_train, X_test, y_train, y_test = train_test_split( X_scaled, y, test_size=0.2, random_state=42)
 
 model = LogisticRegression(class_weight='balanced', max_iter=1000)
 model.fit(X_train, y_train)
@@ -34,17 +31,20 @@ print("Recall:", recall_score(y_test, y_pred))
 print("F1 Score:", f1_score(y_test, y_pred))
 
 # Confusion matrix
-cm = confusion_matrix(y_test, y_pred)
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+cnm = confusion_matrix(y_test, y_pred)
+
+plt.figure(figsize=(5,5))
+sns.heatmap(cnm, annot=True, fmt='d', cmap='Reds')
 plt.xlabel("Predicted")
 plt.ylabel("Actual")
 plt.title("Confusion Matrix")
 plt.show()
 
+
 # Cross validation
 cv = StratifiedKFold(n_splits=9, shuffle=True, random_state=19)
-cv_scores = cross_val_score(model, X_scaled, y, cv=cv, scoring='accuracy')
+cvs = cross_val_score(model, X_scaled, y, cv=cv, scoring='accuracy')
 
-print("Cross Validation Scores:", cv_scores)
-print("Mean CV Accuracy:", cv_scores.mean())
+print("Cross Validation Scores:", cvs)
+print("Mean of  CV Accuracy:", cv_scores.mean())
 
